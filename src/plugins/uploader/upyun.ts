33b9b1072d8f3e54e715897b124f36e2cf83c8a9
terwer <youweics@163.com>
2023-02-10 00:22:29 +0800

    :bug: Fix: fix upyun Electron Date is not allowed because of unsafe behavior

--- src/plugins/uploader/upyun.ts
+++ src/plugins/uploader/upyun.ts
@@ -1,10 +1,13 @@
-import { IPicGo, IPluginConfig, IUpyunConfig, IOldReqOptionsWithFullResponse } from '../../types'
+import {IOldReqOptionsWithFullResponse, IPicGo, IPluginConfig, IUpyunConfig} from '../../types'
 import crypto from 'crypto'
 import MD5 from 'md5'
-import { IBuildInEvent } from '../../utils/enum'
-import { ILocalesKey } from '../../i18n/zh-CN'
-import { safeParse } from '../../utils/common'
+import {IBuildInEvent} from '../../utils/enum'
+import {ILocalesKey} from '../../i18n/zh-CN'
+import {safeParse} from '../../utils/common'
 import mime from 'mime-types'
+// @ts-expect-error
+import upyun from 'upyun'
+import {Readable} from 'stream'
 
 // generate COS signature string
 const generateSignature = (options: IUpyunConfig, fileName: string): string => {
@@ -35,7 +38,7 @@ const postOptions = (options: IUpyunConfig, fileName: string, signature: string,
   }
 }
 
-const handle = async (ctx: IPicGo): Promise<IPicGo> => {
+const handleRest = async (ctx: IPicGo): Promise<IPicGo> => {
   const upyunOptions = ctx.getConfig<IUpyunConfig>('picBed.upyun')
   if (!upyunOptions) {
     throw new Error('Can\'t find upYun config')
@@ -82,59 +85,156 @@ const handle = async (ctx: IPicGo): Promise<IPicGo> => {
   }
 }
 
+const handle = async (ctx: IPicGo): Promise<IPicGo> => {
+  if (!ctx) {
+    await handleRest(ctx)
+  }
+
+  console.warn('Using stream mode for upyun upload, added by terwer, see https://github.com/terwer/Electron-PicGo-Core/blob/dev/src/plugins/uploader/upyun.ts#L88')
+  const upyunOptions = ctx.getConfig<IUpyunConfig>('picBed.upyun')
+  if (!upyunOptions) {
+    throw new Error('Can\'t find upYun config')
+  }
+  try {
+    const serviceName = upyunOptions.bucket
+    const operatorName = upyunOptions.operator
+    const operatorPassword = upyunOptions.password
+
+    // console.log('Before upload,serviceName=>', serviceName)
+    // console.log('Before upload,operatorName=>', operatorName)
+    // console.log('Before upload,operatorPassword=>', operatorPassword)
+    const service = new upyun.Service(serviceName, operatorName, operatorPassword)
+    const client = new upyun.Client(service)
+
+    const imgList = ctx.output
+    for (const img of imgList) {
+      if (img.fileName && img.buffer) {
+        let image = img.buffer
+        if (!image && img.base64Image) {
+          image = Buffer.from(img.base64Image, 'base64')
+        }
+
+        const path = upyunOptions.path || ''
+        const remotePath = `${path}${img.fileName}${upyunOptions.options}`
+        const stream = Readable.from(image)
+        // console.log('Before upload,remotePath=>', remotePath)
+        // console.log('Before upload,stream=>', stream)
+
+        const res = await client.putFile(remotePath, stream)
+        console.log('Using upyun SDK for upload add by terwer, res=>', res)
+
+        if (res) {
+          delete img.base64Image
+          delete img.buffer
+          img.imgUrl = `${upyunOptions.url}/${path}${img.fileName}${upyunOptions.options}`
+        } else {
+          throw new Error('Upload failed')
+        }
+      }
+    }
+
+    return ctx
+  } catch (err: any) {
+    if (err.message === 'Upload failed') {
+      ctx.emit(IBuildInEvent.NOTIFICATION, {
+        title: ctx.i18n.translate<ILocalesKey>('UPLOAD_FAILED'),
+        body: ctx.i18n.translate<ILocalesKey>('CHECK_SETTINGS')
+      })
+    } else {
+      const body = safeParse<{ code: string }>(err.error)
+      ctx.emit(IBuildInEvent.NOTIFICATION, {
+        title: ctx.i18n.translate<ILocalesKey>('UPLOAD_FAILED'),
+        body: ctx.i18n.translate<ILocalesKey>('UPLOAD_FAILED_REASON', {
+          code: typeof body === 'object' ? body.code : body
+        }),
+        text: 'http://docs.upyun.com/api/errno/'
+      })
+    }
+    throw err
+  }
+}
+
 const config = (ctx: IPicGo): IPluginConfig[] => {
   const userConfig = ctx.getConfig<IUpyunConfig>('picBed.upyun') || {}
   const config: IPluginConfig[] = [
     {
       name: 'bucket',
       type: 'input',
-      get alias () { return ctx.i18n.translate<ILocalesKey>('PICBED_UPYUN_BUCKET') },
+      get alias() {
+        return ctx.i18n.translate<ILocalesKey>('PICBED_UPYUN_BUCKET')
+      },
       default: userConfig.bucket || '',
       required: true
     },
     {
       name: 'operator',
       type: 'input',
-      get alias () { return ctx.i18n.translate<ILocalesKey>('PICBED_UPYUN_OPERATOR') },
-      get prefix () {
+      get alias() {
         return ctx.i18n.translate<ILocalesKey>('PICBED_UPYUN_OPERATOR')
       },
-      get message () { return ctx.i18n.translate<ILocalesKey>('PICBED_UPYUN_MESSAGE_OPERATOR') },
+      get prefix() {
+        return ctx.i18n.translate<ILocalesKey>('PICBED_UPYUN_OPERATOR')
+      },
+      get message() {
+        return ctx.i18n.translate<ILocalesKey>('PICBED_UPYUN_MESSAGE_OPERATOR')
+      },
       default: userConfig.operator || '',
       required: true
     },
     {
       name: 'password',
       type: 'password',
-      get prefix () { return ctx.i18n.translate<ILocalesKey>('PICBED_UPYUN_MESSAGE_PASSWORD') },
-      get alias () { return ctx.i18n.translate<ILocalesKey>('PICBED_UPYUN_PASSWORD') },
-      get message () { return ctx.i18n.translate<ILocalesKey>('PICBED_UPYUN_MESSAGE_PASSWORD') },
+      get prefix() {
+        return ctx.i18n.translate<ILocalesKey>('PICBED_UPYUN_MESSAGE_PASSWORD')
+      },
+      get alias() {
+        return ctx.i18n.translate<ILocalesKey>('PICBED_UPYUN_PASSWORD')
+      },
+      get message() {
+        return ctx.i18n.translate<ILocalesKey>('PICBED_UPYUN_MESSAGE_PASSWORD')
+      },
       default: userConfig.password || '',
       required: true
     },
     {
       name: 'url',
       type: 'input',
-      get alias () { return ctx.i18n.translate<ILocalesKey>('PICBED_UPYUN_URL') },
-      get message () { return ctx.i18n.translate<ILocalesKey>('PICBED_UPYUN_MESSAGE_URL') },
+      get alias() {
+        return ctx.i18n.translate<ILocalesKey>('PICBED_UPYUN_URL')
+      },
+      get message() {
+        return ctx.i18n.translate<ILocalesKey>('PICBED_UPYUN_MESSAGE_URL')
+      },
       default: userConfig.url || '',
       required: true
     },
     {
       name: 'options',
       type: 'input',
-      get prefix () { return ctx.i18n.translate<ILocalesKey>('PICBED_UPYUN_OPTIONS') },
-      get alias () { return ctx.i18n.translate<ILocalesKey>('PICBED_UPYUN_OPTIONS') },
-      get message () { return ctx.i18n.translate<ILocalesKey>('PICBED_UPYUN_MESSAGE_OPTIONS') },
+      get prefix() {
+        return ctx.i18n.translate<ILocalesKey>('PICBED_UPYUN_OPTIONS')
+      },
+      get alias() {
+        return ctx.i18n.translate<ILocalesKey>('PICBED_UPYUN_OPTIONS')
+      },
+      get message() {
+        return ctx.i18n.translate<ILocalesKey>('PICBED_UPYUN_MESSAGE_OPTIONS')
+      },
       default: userConfig.options || '',
       required: false
     },
     {
       name: 'path',
       type: 'input',
-      get prefix () { return ctx.i18n.translate<ILocalesKey>('PICBED_UPYUN_PATH') },
-      get alias () { return ctx.i18n.translate<ILocalesKey>('PICBED_UPYUN_PATH') },
-      get message () { return ctx.i18n.translate<ILocalesKey>('PICBED_UPYUN_MESSAGE_PATH') },
+      get prefix() {
+        return ctx.i18n.translate<ILocalesKey>('PICBED_UPYUN_PATH')
+      },
+      get alias() {
+        return ctx.i18n.translate<ILocalesKey>('PICBED_UPYUN_PATH')
+      },
+      get message() {
+        return ctx.i18n.translate<ILocalesKey>('PICBED_UPYUN_MESSAGE_PATH')
+      },
       default: userConfig.path || '',
       required: false
     }
@@ -144,7 +244,9 @@ const config = (ctx: IPicGo): IPluginConfig[] => {
 
 export default function register (ctx: IPicGo): void {
   ctx.helper.uploader.register('upyun', {
-    get name () { return ctx.i18n.translate<ILocalesKey>('PICBED_UPYUN') },
+    get name() {
+      return ctx.i18n.translate<ILocalesKey>('PICBED_UPYUN')
+    },
     handle,
     config
   })
